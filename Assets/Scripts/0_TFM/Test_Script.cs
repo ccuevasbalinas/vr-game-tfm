948bac3a39d68bff6115ae883c348c8751b9a750
pablo ruiz <pablorugo2000@gmail.com>
2023-07-12 17:46:41 +0200

    voicebox and candelights created

---
+++ Assets/Scripts/0_TFM/Test_Script.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Script : MonoBehaviour
+{
+    public void Test()
+    {
+        Debug.Log("Carlos, me tienes hasta el coño.");
+    }
+}
