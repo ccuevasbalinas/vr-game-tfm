7a3094988e8bf89202a2f13e03d16368d772ba85
Carlos Cuevas <ccuevasbalinas@gmail.com>
2023-08-22 18:45:14 +0200

    revisiones morgue y hospital

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,8 +6,11 @@ public class Enemy : MonoBehaviour
 {
     [Header("Enemy Properties")]
     [SerializeField] private float _speed = 2.0f;
+    [SerializeField] private float _range = 8.0f;
     [SerializeField] private Transform _playerTransform;
-    [SerializeField] private bool _followPlayerEnabled = true;
+    private bool _followPlayerEnabled = false;
+    private bool isAlive = true;
+    private bool isWalking = false;
     private Transform _transform;
     private Animator _animator;
     private AudioSource _audioSource;
@@ -17,19 +20,32 @@ public class Enemy : MonoBehaviour
         _transform = GetComponent<Transform>();
         _animator = GetComponent<Animator>();
         _audioSource = GetComponent<AudioSource>();
-        if(_followPlayerEnabled)
-        {
-            _animator.Play("Run");
-        }
-        else
-        {
-            _animator.Play("Idle");
-        }
+        _animator.Play("Idle");
     }
 
     private void Update()
     {
-        FollowPlayer();
+        if(isAlive)
+        {
+            CheckDistanceWithPlayer();
+            FollowPlayer();
+        }
+    }
+
+    private void CheckDistanceWithPlayer()
+    {
+        var distance = Vector3.Distance(transform.position, _playerTransform.position);
+        if(distance < _range)
+        {
+            ActivateFollow();
+            RunAnimation();
+        }
+        else
+        {
+            DeactivateFollow();
+            IdleAnimation();
+        }
+            
     }
 
     private void FollowPlayer()
@@ -41,26 +57,44 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    public void ActivateFollow()
+    private void ActivateFollow()
     {
-        _followPlayerEnabled = true;
-        _animator.Play("Run");
+        _followPlayerEnabled = true;      
+    }
+
+    private void RunAnimation()
+    {
+        if (!isWalking)
+        {
+            isWalking = true;
+            _animator.Play("Run");
+        }
     }
 
     private void DeactivateFollow()
     {
-        _followPlayerEnabled = false;
-        _animator.Play("Scream");
+        _followPlayerEnabled = false;  
+    }
+
+    private void IdleAnimation()
+    {
+        if (isWalking)
+        {
+            isWalking = false;
+            _animator.Play("Idle");
+        }
     }
 
     public void Die()
     {
+        isAlive = false;
         DeactivateFollow();
+        _animator.Play("Scream");
         PlayEnemySound();
         StartCoroutine(DieCoroutine());
     }
 
-    public void PlayEnemySound()
+    private void PlayEnemySound()
     {
         _audioSource.Play();
     }
